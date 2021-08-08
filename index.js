0f9ff024d8a6fec65c2811377394fd0911c08cba
tea-loves-jkdyd <aditi.6ti@gmail.com>
2021-08-08 23:51:56 +0530

    minor changes

---
+++ index.js
@@ -0,0 +1,11 @@
+function validateForm(){
+	let nameVal = document.getElementById("name").value;
+	try{
+		if(nameVal=""){
+			throw "Warning: Please enter your name!";
+		}
+	}catch(e){
+		document.getElementById("nameMsg").innerHTML= e;
+	}
+
+}
